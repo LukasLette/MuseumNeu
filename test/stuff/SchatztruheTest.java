c1d538dde7566fde2ff82efe27837cf51bffd381
LucasLette <l.preusse_de_vieira@edu.letteverein.berlin>
2022-10-14 13:59:08 +0200

    tests

--- test/stuff/SchatztruheTest.java
+++ test/stuff/SchatztruheTest.java
@@ -1,9 +1,37 @@
 package stuff;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
 public class SchatztruheTest {
 
+    @Test
+    public void testeBezeichnungen() {
+
+        Collection<Schmuck> schatz = Schatztruhe.getSchatz();
+
+        for (Schmuck schmuck : schatz) {
+
+            String bezeichnung = schmuck.getBezeichnung();
+            Assert.assertNotNull("Die Bezeichnung von " + schmuck.getClass().getName() + " ist null", bezeichnung);
+            Assert.assertTrue("Die Bezeichnung von " + schmuck.getClass().getName() + " ist zu klein",bezeichnung.length() > 3);
+        }
+    }
+        @Test
+        public void testeMaterial() {
+
+            Collection<Schmuck> schatz = Schatztruhe.getSchatz();
+
+            for (Schmuck schmuck : schatz) {
+
+                Material material = schmuck.getMaterial();
+                Assert.assertNotNull("Die Bezeichnung von " + schmuck.getClass().getName() + " ist null", bezeichnung);
+            }
+    }
 
 
 
